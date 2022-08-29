75820f497fdbfe2faa72480b0354299355feb5c2
jinguang <chjg1231@gmail.com>
2022-08-29 15:41:01 +0900

    모델 틀

--- src/models/index.js
+++ src/models/index.js
@@ -2,14 +2,22 @@ export * from "./sequelize.js";
 import sequelize from "./sequelize.js";
 
 import User from "./user.js"
+import Tag from "./tag.js"
+import UserTag from "./usertag.js"
 
 const db = {};
 
 db.sequelize = sequelize;
 db.User = User;
+db.Tag = Tag;
+db.UserTag = Tag;
 
 User.init(sequelize);
+Tag.init(sequelize);
+UserTag.init(sequelize);
 
 User.associate(db);
+Tag.associate(db);
+UserTag.associate(db);
 
 export { db };
