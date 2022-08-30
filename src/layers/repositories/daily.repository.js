04ea2f23bcb5746eb6c22976fe210f77fcd49220
jinguang <chjg1231@gmail.com>
2022-08-30 12:01:49 +0900

    FEAT 데일리 태그 리스트 기능 #14

--- src/layers/repositories/daily.repository.js
+++ src/layers/repositories/daily.repository.js
@@ -7,10 +7,17 @@ class dailycontroller {
   };
 
   taglist = async (userId) => {
-    const mytaglist = await UserTag.findAll({ where: { userId } });
+    const mytaglist = await UserTag.findAll({
+      where: { UserUserId: userId },
+      include: {
+        model: Tag,
+        attributes: ["tagname"],
+      },
+    });
+    return mytaglist;
   };
 
-  taglist = async (userId, usertagId, currentDate) => {
+  schedule = async (userId, usertagId, currentDate) => {
     const tag = await UserTag.findOne({ where: { userId, usertagId } });
     await Schedule.craete(
       { timeCycle, weekCycle },
