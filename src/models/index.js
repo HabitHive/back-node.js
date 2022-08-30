412e70b1ec19bbf997518136dc89f57e157aa261
SoularOfDawn <tntkala001@gmail.com>
2022-08-30 10:37:48 +0900

    UPDATE Models Pet part

--- src/models/index.js
+++ src/models/index.js
@@ -1,15 +1,19 @@
 export * from "./sequelize.js";
 import sequelize from "./sequelize.js";
 
-import User from "./user.js"
+import User from "./user.js";
+import Pet from "./pet.js";
 
 const db = {};
 
 db.sequelize = sequelize;
 db.User = User;
+db.Pet = Pet;
 
 User.init(sequelize);
+Pet.init(sequelize);
 
 User.associate(db);
+Pet.associate(db);
 
 export { db };
--- src/models/pet.js
+++ src/models/pet.js
@@ -1,25 +1,32 @@
 import Sequelize from "sequelize";
-
 import { sequelize } from "./sequelize.js";
 
-export default class User extends Sequelize.Model {
+export default class Pet extends Sequelize.Model {
   static init(sequelize) {
     return super.init(
-      {},
+      {
+        user_id: {
+          type: Sequelize.INTEGER,
+        },
+        type: Sequelize.INTEGER,
+        pet_name: Sequelize.STRING,
+        level: Sequelize.INTEGER,
+        exp: Sequelize.INTEGER,
+      },
       {
         sequelize,
         timestamps: true,
         paranoid: false,
         underscored: false,
-        modelName: "User",
-        tableName: "user",
+        modelName: "Pet",
+        tableName: "pet",
         charset: "utf8",
         collate: "utf8_general_ci",
       }
     );
   }
   static associate(db) {
-    db.User.hasMany(db.Pet, {
+    db.Pet.belongsTo(db.User, {
       foreignKey: { name: "user_id", allowNull: false },
       onDelete: "cascade",
       onUpdate: "cascade",
