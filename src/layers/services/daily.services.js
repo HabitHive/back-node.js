64dc87a4202e36bf47c1eccb08215d751ccdba8d
jinguang <chjg1231@gmail.com>
2022-09-01 12:32:52 +0900

    close #11 #12 #13 #20 데일리 페이지와 스케줄 설정 패이지의 정리

--- src/layers/services/daily.services.js
+++ src/layers/services/daily.services.js
@@ -11,13 +11,16 @@ export default class dailyServices {
     // endDate.setDate(endDate.getDate() - 30);
     // todayDate.setDate(todayDate.getDate() - 4)
     const dailyTagLists = result.map((list) => {
-      if (todayDate <= todayDate <= todayDate) {
-        console.log(todayDate.getDay()); //나중에 weekCycle 로 제한 하기&& todayDate.getDay()
-        return {
-          userTagId: list.id,
-          tagName: list.Tag.tagName,
-          // timeCycle: list.schedule.timeCycle,
-        };
+      if (list.startDate <= todayDate <= list.endDate) {
+        if (list.schedule.weekCycle.includes(todayDate.getDay())) {
+          console.log(list.schedule.weekCycle.includes(todayDate.getDay())); //나중에 weekCycle 로 제한 하기&& todayDate.getDay()
+          return {
+            userTagId: list.id,
+            tagName: list.Tag.tagName,
+            scheduleId: list.schedule.id,
+            timeCycle: list.schedule.timeCycle,
+          };
+        }
       }
     });
     //현재 날짜의 대해 유효기간안에 있고 (startDate > 현재날짜 < endDate)
@@ -35,7 +38,7 @@ export default class dailyServices {
       return {
         userTagId: list.id,
         tagName: list.Tag.tagName,
-        period: list.period, // 남은 날짜에 따른 값을 확인해서 배출
+        period: list.period,
       };
     });
     return tagLists;
@@ -43,21 +46,28 @@ export default class dailyServices {
 
   schedulePage = async (userId, userTagId) => {
     const result = await this.dailyRepositories.schedulePage(userId, userTagId);
-    let date = result.startDate;
-    let period = result.period / 1;
-    // date = new Date(); //현재 시간
-    date.setDate(date.getDate() + period);
-    return date;
+    // let date = result.startDate;
+    // let period = result.period / 1;
+    // // date = new Date(); //현재 시간
+    // date.setDate(date.getDate() + period);
+    return result;
   };
 
-  schedule = async (userId, userTagId, timeCycle, weekCycle) => {
-    const result = await this.dailyRepositories.schedule(
+  schedule = async (userId, userTagId, startDate, timeCycle, weekCycle) => {
+    // 스케줄 처음 생성만 고려; 나중에 수정할때 시작시간과 현제시간의 문제 잘 생각하기
+    const tagCheck = await this.dailyRepositories.scheduleTagCheck(
+      userId,
+      userTagId
+    );
+    let date = result.startDate;
+    const period = tagCheck.period / 1;
+    const endDate = date.setDate(date.getDate() + period);
+    await this.dailyRepositories.scheduleStartDate(
       userId,
       userTagId,
-      timeCycle,
-      weekCycle
+      startDate,
+      endDate
     );
-
-    return result;
+    await this.dailyRepositories.schedule(userTagId, timeCycle, weekCycle);
   };
 }
