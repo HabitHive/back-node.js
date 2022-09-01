7ef39315d64c358a09e3c7919996f0e43cfa6ca6
maneuljokbal <glory0824@naver.com>
2022-09-01 17:11:08 +0900

    DESC: 테스트 코드 주석 처리

--- src/layers/routers/index.js
+++ src/layers/routers/index.js
@@ -1,10 +1,10 @@
 import express from "express";
 import UserRouter from "./user.router.js";
-import Test from "./test.js";
+// import Test from "./testrouter.js";
 
 const router = express.Router();
 
 router.use("/user", UserRouter);
-router.use("/test", Test);
+// router.use("/test", Test);
 
 export default router;
