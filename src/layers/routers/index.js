44cb92214d5d610e18fdbc3a099d75f9ccf63412
maneuljokbal <glory0824@naver.com>
2022-09-01 17:38:12 +0900

    MODIFY: 주석처리

--- src/layers/routers/index.js
+++ src/layers/routers/index.js
@@ -1,10 +1,14 @@
 import express from "express";
 import UserRouter from "./user.router.js";
+// import PetRouter from "./pet.router.js";
+// import TagRouter from "./tag.router.js";
 // import Test from "./testrouter.js";
 
 const router = express.Router();
 
 router.use("/user", UserRouter);
+// router.use("/pet", PetRouter);
+// router.use("/tag", TagRouter);
 // router.use("/test", Test);
 
 export default router;
