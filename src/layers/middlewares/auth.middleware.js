4d270690d456edff8187d569cc798227951c7d05
maneuljokbal <glory0824@naver.com>
2022-09-02 16:23:33 +0900

    STYLE: 변수명 lowerCamelCase 적용

--- src/layers/middlewares/auth.middleware.js
+++ src/layers/middlewares/auth.middleware.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 export default async (req, res, next) => {
   let { authorization } = req.headers; //header 안쪽 authorization(구조분해 할당)라는 key값으로 엑세스 토큰 받아오기
-  let refreshtoken = req.session.a1; //리프레쉬 토큰 값 불러오기
+  let refreshToken = req.session.a1; //리프레쉬 토큰 값 불러오기
 
   try {
     //엑세스 토큰 존재 유무 확인
@@ -21,12 +21,12 @@ export default async (req, res, next) => {
     }
 
     //리프레쉬 토큰 존재 유무 확인
-    if (refreshtoken === undefined) {
+    if (refreshToken === undefined) {
       throw new Error("not exist token");
     }
 
     //엑세스 토큰 유효성 검사
-    const access_token_verify = jwt.verify(
+    const accessTokenVerify = jwt.verify(
       tokenValue,
       process.env.ACCESS_TOKEN_SECRET,
       (err, decoded) => {
@@ -34,8 +34,8 @@ export default async (req, res, next) => {
         if (err) {
           //엑세스 토큰의 유효기간 만기시 재발급 과정 진행
           if (err.name === "TokenExpiredError") {
-            const refresh_token_verify = jwt.verify(
-              refreshtoken,
+            const refreshTokenVerify = jwt.verify(
+              refreshToken,
               process.env.REFRESH_TOKEN_SECRET,
               (err, decoded) => {
                 if (err) {
@@ -49,38 +49,38 @@ export default async (req, res, next) => {
             );
 
             //로그인시 생성된 엑세스 토큰 값, 현재 헤더 내 토큰 값
-            const proven_access_tokenValue = refresh_token_verify.key2;
-            const [tokenType, now_access_tokenValue] = (
+            const provenAccessTokenValue = refreshTokenVerify.key2;
+            const [tokenType, nowAccessTokenValue] = (
               authorization || ""
             ).split(" ");
 
             //로그인시 생성된 엑세스 토큰 값과 헤더 내 엑세스 토큰 값 비교
 
             //같을 시
-            if (proven_access_tokenValue === now_access_tokenValue) {
-              const new_access_token = jwt.sign(
-                { key1: refresh_token_verify.key3 + parseInt(process.env.SUM) },
+            if (provenAccessTokenValue === nowAccessTokenValue) {
+              const newAccessToken = jwt.sign(
+                { key1: refreshTokenVerify.key3 + parseInt(process.env.SUM) },
                 process.env.ACCESS_TOKEN_SECRET,
                 {
                   expiresIn: "1h",
                 }
               );
-              const new_refresh_token = jwt.sign(
+              const newRefreshToken = jwt.sign(
                 {
-                  key2: new_access_token,
-                  key3: refresh_token_verify.key3,
+                  key2: newAccessToken,
+                  key3: refreshTokenVerify.key3,
                 },
                 process.env.REFRESH_TOKEN_SECRET,
                 { expiresIn: "7d" }
               );
-              req.headers.authorization = `Bearer ${new_access_token}`;
-              req.session.a1 = new_refresh_token;
+              req.headers.authorization = `Bearer ${newAccessToken}`;
+              req.session.a1 = newRefreshToken;
               req.session.save();
               console.log(req.headers.authorization);
               console.log(req.session.a1);
 
-              const decoded_value = jwt.decode(now_access_tokenValue);
-              return decoded_value;
+              const decodedValue = jwt.decode(nowAccessTokenValue);
+              return decodedValue;
             }
             //다를 시
             else {
@@ -102,17 +102,17 @@ export default async (req, res, next) => {
     );
 
     //반환된 엑세스 토큰내 유저 아이디 검색
-    const find_user = await User.findOne({
-      where: { user_id: access_token_verify.key1 - parseInt(process.env.SUM) },
+    const findUser = await User.findOne({
+      where: { user_id: accessTokenVerify.key1 - parseInt(process.env.SUM) },
     });
 
     //유저아이디 존재하지 않을시 에러 반환 처리
-    if (find_user === null) {
+    if (findUser === null) {
       throw new Error("invalid signature");
     }
 
     //유저 존재 확인 후 반환된 엑세스 토큰내 유저 아이디 로컬에 저장
-    res.locals.user_id = access_token_verify.key1 - parseInt(process.env.SUM);
+    res.locals.user_id = accessTokenVerify.key1 - parseInt(process.env.SUM);
 
     //여기까지 왔다면 모든 검증 완료 이대로 next()로 다음 단계로 넘어감
     next();
