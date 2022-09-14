fc67a8c7493670c48a591ee50454c443204b0172
maneuljokbal <glory0824@naver.com>
2022-09-14 20:02:54 +0900

    MODIFY: 미들웨어 올바르게 수정

--- src/layers/utils/auth.middleware.js
+++ src/layers/utils/auth.middleware.js
@@ -1,34 +1,14 @@
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 import User from "../../models/user.js";
-import Session from "../../models/session.js";
 dotenv.config();
 
 export default async (req, res, next) => {
   try {
     // 요청 헤더에서 토큰 값을 가지고 옴
     const { authorization } = req.headers; // 구조분해 할당
-    const { session } = req.headers;
     const [tokenType, tokenValue] = (authorization || "").split(" ");
-    const sessionData = await Session.findOne({
-      where: { session_id: session },
-      raw: true,
-    });
-    const refreshToken = sessionData.data.a1; // 리프레쉬 토큰값
-    console.log(sessionData.data.cookie);
     const user = jwt.verify(tokenValue, process.env.ACCESS_TOKEN_SECRET); // 받아온 엑세스 토큰 값을 검증
-    const verifyUser = jwt.verify(
-      refreshToken,
-      process.env.REFRESH_TOKEN_SECRET,
-      (err, decoded) => {
-        if (err) {
-          const error = new Error("verify token error");
-          error.name = "invalid token";
-          throw error;
-        }
-        return decoded;
-      }
-    ); // 받아온 리프레쉬 토큰 값을 검증해서 verifyUser에 저장
 
     const existUser = await User.findOne({
       where: { user_id: user.key1 - parseInt(process.env.SUM) },
@@ -45,20 +25,6 @@ export default async (req, res, next) => {
     if (tokenType !== "Bearer") {
       const error = new Error("Token error");
       error.name = "wrong token";
-      throw error;
-    }
-
-    // 처음 발급한 엑세스 토큰값이랑 비교 후 틀리다면 에러 반환
-    if (tokenValue !== verifyUser.key2) {
-      const error = new Error("Token error");
-      error.name = "does not match token";
-      throw error;
-    }
-
-    // 세션 확인 없다면 에러 throw
-    if (refreshToken === undefined) {
-      const error = new Error("Session error");
-      error.name = "not login";
       throw error;
     }
 
